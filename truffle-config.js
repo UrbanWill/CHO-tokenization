a8c134647ecd6a3eac3fcef9339c3e72146b458a
will <willian.urban@gmail.com>
2021-08-10 17:32:39 +0900

    installed hdwallet, added ganache_local to truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require("dotenv").config({ path: "./.env" });
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,6 +11,16 @@ module.exports = {
     development: {
       port: 7545,
       host: "127.0.0.1",
+      network_id: 1337,
+    },
+    ganache_local: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "http://127.0.0.1:7545",
+          MetaMaskAccountIndex
+        );
+      },
       network_id: 1337,
     },
   },
